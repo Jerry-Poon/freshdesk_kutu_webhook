9886c50615aba3621fa9a4ebd586c1fa6323772c
Jerry Poon <jerry.poon@kickscrew.com>
2023-12-25 22:58:43 +0800

    adding logging info

--- src/util.py
+++ src/util.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, 
 from sqlalchemy.orm import Session
 import pandas as pd
 import logging
+import asyncio
+
 logging.basicConfig(level=logging.INFO)
 
 
@@ -10,13 +12,13 @@ class dbConnector:
         self.engine=create_engine(f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}', echo=False)
         self.cnx = self.engine.connect()
 
-    async def getTableData(self, table_name: str) -> pd.DataFrame:
+    def getTableData(self, table_name: str) -> pd.DataFrame:
         return pd.read_sql(f'SELECT * FROM {table_name}', self.cnx)
     
-    async def insertTableData(self, table_name: str, df: pd.DataFrame) -> None:
+    def insertTableData(self, table_name: str, df: pd.DataFrame) -> None:
         return df.to_sql(name=table_name, con=self.cnx, if_exists='append', index=False)
     
-    async def test_query(self, target_table):
+    def test_query(self, target_table):
         table_object = Table(target_table, MetaData(),autoload_replace=True, autoload_with=self.engine)
 
 
--- unittest/unittest_post_cancel_order_number.py
+++ unittest/unittest_post_cancel_order_number.py
@@ -2,7 +2,7 @@ import pandas as pd
 from io import StringIO
 
 json_payload = """
-{"order_number": "KS123455",   "status": "cancelled",   "processed": true,   "cancel_datetime": "2023-12-16 16:15:04" }
+{"order_number": "KS123455",   "status": "cancelled",   "processed": true,   "cancel_datetime": "2023-12-25 16:15:04", "udt": "2023-12-25 10:41:04" }
 """
 
 df = pd.read_json(StringIO(json_payload), orient='index').T
