6cf9dad6bca028688fabc19adbd44900189c75d3
Jerry Poon <jerry.poon@kickscrew.com>
2023-12-26 11:35:56 +0800

    Remove payload timezone info

--- unittest/unittest_post_cancel_order_number.py
+++ unittest/unittest_post_cancel_order_number.py
@@ -2,9 +2,17 @@ import pandas as pd
 from io import StringIO
 
 json_payload = """
-{"order_number": "KS123455",   "status": "cancelled",   "processed": true,   "cancel_datetime": "2023-12-25 16:15:04", "udt": "2023-12-25 10:41:04" }
+{
+"order_number": "KS9999999",
+"status": "cancelled",
+"processed": false,
+"cancel_datetime": "2023-12-26 11:29:30 +0800",
+"udt": "2023-12-26 11:29:30 +0800"
+}
 """
 
 df = pd.read_json(StringIO(json_payload), orient='index').T
+for x in ['cancel_datetime', 'udt']:
+    df[x] = df[x].apply(lambda x: x.replace(' +0800', ''))
 print(df['order_number'][0])
 breakpoint()
